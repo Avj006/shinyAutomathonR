f371ef6292e70da8129e13abbf3e2a43a659840a
Andrea Villaseñor Jiménez <a01642088@tec.mx>
2026-05-24 14:34:01 -0600

    codigo del profe

---
+++ app.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input text area
+      textAreaInput(inputId = "myinputtext",
+                  label = "Write something here:"
+                  )
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Display selected number of bins.
+      verbatimTextOutput("outputtext"),
+      
+    )
+  )
+)
+
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  
+  output$outputtext <- renderText({
+    paste0("Output: ", input$myinputtext)
+  })
+  
+}
+
+# Run the app.
+shinyApp(ui = ui, server = server)
